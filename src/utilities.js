5e6e9531a9155cb9d7a515de682be309a9ed5459
Hliiv <165692608+Hliiv@users.noreply.github.com>
2024-06-05 19:06:16 +0300

    weather

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,5 @@
+
+
 async function fetchWeatherData({lat, long}) {
     const requestUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`
     const response = await fetch(requestUrl)
@@ -8,3 +10,4 @@ async function fetchWeatherData({lat, long}) {
 export {
     fetchWeatherData
 }
+
